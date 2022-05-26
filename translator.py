a76f1227d8dd92e1b3fcec63c23c582e92f284aa
srividya16496 <srividya.mukundan@gmail.com>
2022-05-26 12:29:59 +0530

    Update translator.py

--- translator.py
+++ translator.py
@@ -4,8 +4,8 @@ from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 # Set some variables
-api_key = 'nG0iX-KPotlqCO9vVJyoGJ4JINKCf7UejygK19IVxaQ7'
-api_url = 'https://api.us-south.language-translator.watson.cloud.ibm.com/instances/ab732265-4699-4c56-9ee5-c7e37c110fd3'
+api_key = '[API KEY]'
+api_url = '[API URL]'
 model_id = 'en-it'
 text_to_translate = 'Your content you want translate here'
 
